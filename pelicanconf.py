df8a02e2b45d627af805e615174f42dfb8875725
Samuel Colburn <crashoverripe@gmail.com>
2022-11-06 01:12:48 -0500

    initial

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Crash Overripe'
+SITENAME = 'overripe.net'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Pelican', 'https://getpelican.com/'),
+    ('Python.org', 'https://www.python.org/'),
+    ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('@crash@infosec.exchange', 'https://infosec.exchange/@crash'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
